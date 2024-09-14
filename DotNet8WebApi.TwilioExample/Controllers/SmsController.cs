3cc4d91b452c92bef32266df93950624d748caf1
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-14 22:43:00 +0630

    Add project files.

---
+++ DotNet8WebApi.TwilioExample/Controllers/SmsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Twilio.Types;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace DotNet8WebApi.TwilioExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SmsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SmsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendSms()
+        {
+            var accountSid = "";
+            var authToken = "";
+            TwilioClient.Init(accountSid, authToken);
+
+            var messageOptions = new CreateMessageOptions(
+              new PhoneNumber("+959773871112"));
+            messageOptions.From = new PhoneNumber("+12073062974");
+            messageOptions.Body = "Hello from Twilio";
+
+
+            var message = MessageResource.Create(messageOptions);
+            Console.WriteLine(message.Body);
+
+            return Ok(message);
+        }
+    }
+}
