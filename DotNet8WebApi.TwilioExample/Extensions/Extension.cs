6f7b073bf792331068a380f552469a9ac3631edc
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-15 00:39:26 +0630

    [ Added ] Extension

---
+++ DotNet8WebApi.TwilioExample/Extensions/Extension.cs
@@ -0,0 +1,31 @@
+﻿using DotNet8WebApi.TwilioExample.Entities;
+using DotNet8WebApi.TwilioExample.Features.Auth.Register;
+
+namespace DotNet8WebApi.TwilioExample.Extensions
+{
+    public static class Extension
+    {
+        public static Tbl_User ToEntity(this RegisterRequestDTO registerRequest)
+        {
+            return new Tbl_User
+            {
+                UserId = Ulid.NewUlid().ToString(),
+                UserName = registerRequest.UserName,
+                PhoneNumber = registerRequest.PhoneNumber,
+                Password = registerRequest.Password,
+                DeleteFlag = false
+            };
+        }
+
+        public static Tbl_Setup ToEntity(string userId, string otp)
+        {
+            return new Tbl_Setup
+            {
+                SetupId = Ulid.NewUlid().ToString(),
+                UserId = userId,
+                OtpCode = otp,
+                CreatedDate = DateTime.Now
+            };
+        }
+    }
+}
--- DotNet8WebApi.TwilioExample/Features/Auth/Register/RegisterCommandHandler.cs
+++ DotNet8WebApi.TwilioExample/Features/Auth/Register/RegisterCommandHandler.cs
@@ -1,10 +1,18 @@
-﻿using DotNet8WebApi.TwilioExample.Entities;
+﻿using DotNet8WebApi.TwilioExample.Db;
+using DotNet8WebApi.TwilioExample.Entities;
 using MediatR;
 
 namespace DotNet8WebApi.TwilioExample.Features.Auth.Register
 {
     public class RegisterCommandHandler : IRequestHandler<RegisterCommand, Result<AuthDTO>>
     {
+        private readonly AppDbContext _context;
+
+        public RegisterCommandHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public Task<Result<AuthDTO>> Handle(RegisterCommand request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
